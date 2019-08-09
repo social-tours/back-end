3bbe107971b43c27876e618cae14d4895cf053fe
Louis Magdaleno <buildthatapp@gmail.com>
2019-08-08 20:16:33 -0700

    Added token service

---
+++ auth/tokenService.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken"); // installed this library
+
+const secret = process.env.JWT_SECRET;
+
+module.exports = {
+  generateToken
+};
+
+/**
+ * Creates a new jwt for the specific user
+ * @param user
+ * @returns {*} signed jwt
+ */
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    email: user.email
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
