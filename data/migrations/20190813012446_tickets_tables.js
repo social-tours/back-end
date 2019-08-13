df8e5bb1b2d2766e25d4f1aad98bcb7a87fc2c69
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-08-13 01:39:46 -0400

    Add Tickets and TicketTypes tables to db schema

---
+++ data/migrations/20190813012446_tickets_tables.js
@@ -0,0 +1,26 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('TicketTypes', tbl => {
+    tbl.increments('id')
+    tbl.string('title', 255).notNullable()
+    tbl.decimal('price')
+    tbl.timestamp('created_at').defaultTo(knex.fn.now())
+    tbl.timestamp('updated_at')
+  })
+
+  await knex.schema.createTable('Tickets', tbl => {
+    tbl.increments('id')
+    tbl.integer('type')
+      .references('id')
+      .inTable('TicketTypes')
+    tbl.integer('user_id')
+      .references('id')
+      .inTable('Users')
+    tbl.integer('event_id')
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('Tickets')
+  await knex.schema.dropTableIfExists('TicketTypes')
+};
