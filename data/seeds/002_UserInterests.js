2530701f48aa7f38a6c2c9094115cf9a038b8eca
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-08-15 01:57:52 -0400

    Add test data seed for Interests and UserInterests tables

--- data/seeds/002_UserInterests.js
+++ data/seeds/002_UserInterests.js
@@ -1,7 +1,15 @@
 exports.seed = async function(knex) {
-  await knex('interests').insert([
+  await knex('Interests').insert([
     { id: 1,
       name: 'Netflix Binging',
+    },
+    {
+      id: 2,
+      name: 'Weightlifting'
+    },
+    {
+      id: 3,
+      name: 'Music'
     }
   ])
 }
@@ -11,6 +19,14 @@ exports.seed = async function(knex) {
     { id: 1,
       user_id: 2,
       interest_id: 1
+    },
+    { id: 2,
+      user_id: 3,
+      interest_id: 2
+    },
+    { id: 3,
+      user_id: 1,
+      interest_id: 3
     }
   ])
 }
