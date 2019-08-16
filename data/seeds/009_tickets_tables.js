4adc9be44bd33456b2fa619aa28734d7efb01c4b
GregoryUrban <turknben@gmail.com>
2019-08-15 21:47:12 -0400

    Coded 009 seed ticket tables

--- data/seeds/009_tickets_tables.js
+++ data/seeds/009_tickets_tables.js
@@ -1,13 +1,22 @@
+exports.seed = async function(knex) {
+  await knex('TicketTypes').insert([
+    { id: 1,
+      title: "1", // FK ID in 'Tickets' table
+      price: 49.99,
+      created_at: 'August 14 2019',
+      updated_at: 'August 15 2019'
+    } 
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  ])
+}
+
+exports.seed = async function(knex) {
+  await knex('Tickets').insert([
+    { id: 1,
+      type: 1, // FK ID in 'TicketTypes' table
+      user_id: 1, // FK ID in 'Users' table
+      event_id: 1
+    } 
+
+  ])
+}
