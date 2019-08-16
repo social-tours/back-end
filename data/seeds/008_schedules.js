a474a2afe0700c86e124ccf1684bc3f70634cbcd
GregoryUrban <turknben@gmail.com>
2019-08-15 21:54:06 -0400

    Coded seeds 008 schedules

--- data/seeds/008_schedules.js
+++ data/seeds/008_schedules.js
@@ -1,13 +1,15 @@
+exports.seed = async function(knex) {
+  await knex('Schedules').insert([
+    { id: 1,
+      event_id: 1, // FK ID in 'Events' table
+      sequence: 1,
+      title: 'Bifunkal',
+      description: 'Blues band from Chicago',
+      start_time: '05:00 PM',
+      end_time: 'October 15 2019',
+      created_at: 'August 14 2019',
+      updated_at: 'August 15 2019'
+    } 
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  ])
+}
