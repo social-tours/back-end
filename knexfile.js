fc129e5cbe8c90da5a2154e5709efbf2285e3a38
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-08-20 12:44:09 +0800

    Refactor knexfile to prettier format

--- knexfile.js
+++ knexfile.js
@@ -1,69 +1,67 @@
-require('dotenv').config() // activate module to read environmental variables
+require("dotenv").config(); // activate module to read environmental variables
 
 module.exports = {
+	development: {
+		client: "pg",
+		useNullAsDefault: true,
+		connection: {
+			connectionString: process.env.DEV_DB_URL,
+			ssl: true
+		},
+		migrations: {
+			directory: "./data/migrations"
+		},
+		seeds: {
+			directory: "./data/seeds"
+		},
+		debug: true
+	},
 
-  development: {
-    client: 'pg',
-    useNullAsDefault: true,
-    connection: {
-      connectionString: process.env.DEV_DB_URL,
-      ssl: true
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    },
-    debug: true
-  },
+	testing: {
+		client: "pg",
+		useNullAsDefault: true,
+		connection: {
+			connectionString: process.env.TEST_DB_URL,
+			ssl: true
+		},
+		migrations: {
+			directory: "./data/migrations"
+		},
+		seeds: {
+			directory: "./data/seeds/testing" // disposable generic boilerplate tables, not dev data
+		},
+		debug: false // too verbose of an operation
+	},
 
-    testing: {
-    client: 'pg',
-    useNullAsDefault: true,
-    connection: {
-      connectionString: process.env.TEST_DB_URL,
-      ssl: true
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds/testing' // disposable generic boilerplate tables, not dev data
-    },
-    debug: false // too verbose of an operation
-    },
+	staging: {
+		client: "pg",
+		useNullAsDefault: true,
+		connection: {
+			connectionString: process.env.STAGING_DB_URL,
+			ssl: true
+		},
+		migrations: {
+			directory: "./data/migrations"
+		},
+		seeds: {
+			directory: "./data/seeds"
+		},
+		debug: true // could be either
+	},
 
-  staging: {
-    client: 'pg',
-    useNullAsDefault: true,
-    connection: {
-      connectionString: process.env.STAGING_DB_URL,
-      ssl: true
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    },
-    debug: true // could be either
-  },
-
-  production: {
-    client: 'pg',
-    useNullAsDefault: true,
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    },
-    debug: false
-  }
-
+	production: {
+		client: "pg",
+		useNullAsDefault: true,
+		connection: {
+			connectionString: process.env.DATABASE_URL,
+			ssl: true
+		},
+		migrations: {
+			directory: "./data/migrations"
+		},
+		seeds: {
+			directory: "./data/seeds"
+		},
+		debug: false
+	}
 };
