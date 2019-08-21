125df409b3bd4de32c92a172c347166bffcd8942
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-08-21 13:42:13 +0800

    Implement findById model
    Implement addRecord model

--- data/models/index.js
+++ data/models/index.js
@@ -6,7 +6,30 @@ function findAll(table) {
 	return db(table).orderBy("id");
 }
 
+async function findById(table, id) {
+	let result = await db(table)
+		.where({ id })
+		.first();
+
+	return result;
+}
+
+async function addRecord(table, data) {
+	try {
+		const [results] = await db(table)
+			.insert(data)
+			.returning("*");
+		console.log(`addRecord results`, results);
+
+		return results;
+	} catch (err) {
+		return err;
+	}
+}
+
 module.exports = {
 	db,
-	findAll
+	findAll,
+	findById,
+	addRecord
 };
--- data/models/models.spec.js
+++ data/models/models.spec.js
@@ -25,7 +25,7 @@ describe("Models testing", () => {
 		}
 	});
 
-	describe(`findAll()`, () => {
+	describe("findAll()", () => {
 		it("find all records in specified tables", async () => {
 			// Seed with test data
 			const userTypes = [
@@ -89,6 +89,88 @@ describe("Models testing", () => {
 			//Validate Model
 			expect(fetchUserTypes.length).toBe(2);
 			expect(fetchUsers[1].user_name).toEqual(testUsers[1].user_name);
+		});
+	});
+
+	describe("findById()", () => {
+		it("find record by id", async () => {
+			// Seed with test data
+			const userTypes = [
+				{ id: 1, description: "Influencer" },
+				{ id: 2, description: "Follower" }
+			];
+
+			await db("UserTypes").insert(userTypes);
+
+			// Run model
+			const fetchRecord = await Models.findById("UserTypes", 2);
+
+			// Validate Model
+			expect(fetchRecord.description).toEqual("Follower");
+		});
+	});
+
+	describe("addRecord", () => {
+		it("add records into the database", async () => {
+			// Seed with test data
+			const userTypes = [
+				{ id: 1, description: "Influencer" },
+				{ id: 2, description: "Follower" }
+			];
+
+			const testUsers = [
+				{
+					id: 1,
+					first_name: "Yana",
+					last_name: "Blake",
+					user_name: "yblake",
+					gender: "Female",
+					birth_date: "1987-09-17",
+					city: "Dallas",
+					state_province: "TX",
+					country: "USA",
+					email: "yana@yogiyana.com",
+					password: bcrypt.hashSync("pass", 12),
+					type: 1
+				},
+				{
+					id: 2,
+					first_name: "George",
+					last_name: "Kaplam",
+					user_name: "gkaplan",
+					gender: "Male",
+					birth_date: "1955-11-05",
+					city: "Hill Valley",
+					state_province: "CA",
+					country: "USA",
+					email: "george@gmail.com",
+					password: bcrypt.hashSync("pass", 12),
+					type: 2
+				},
+				{
+					id: 3,
+					first_name: "Cindy",
+					last_name: "Brady",
+					user_name: "cbrady",
+					gender: "Female",
+					birth_date: "1995-10-05",
+					city: "Chicago",
+					state_province: "IL",
+					country: "USA",
+					email: "cindy@selfiequeen.com",
+					password: bcrypt.hashSync("pass", 12),
+					type: 1
+				}
+			];
+
+			// Run model
+			await Models.addRecord("UserTypes", userTypes);
+			await Models.addRecord("Users", testUsers);
+
+			// Validate model
+			const data = await db("Users");
+			expect(data).toHaveLength(testUsers.length);
+			expect(data[1].first_name).toBe("George");
 		});
 	});
 });
