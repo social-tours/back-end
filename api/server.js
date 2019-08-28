1ad8de4a757489c450273904673f6018ae3dd8d7
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-08-28 17:40:30 +0800

    Add basic landing page

--- api/server.js
+++ api/server.js
@@ -5,19 +5,23 @@ const cors = require("cors");
 const helmet = require("helmet");
 
 const userRoutes = require("../routes/userRoutes");
-const eventRoutes = require('../routes/eventRoutes');
+const eventRoutes = require("../routes/eventRoutes");
 
 const server = express();
 
 server.use(express.json(), helmet(), cors());
 
 // Sanity Check
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'up' });
+server.get("/", (req, res) => {
+	res.status(200).json({ api: "up" });
+});
+
+server.use("/api", (req, res) => {
+	res.send(`<center><h1>Welcome to the Social Tours API</h1></center>`);
 });
 
 // Route handling
-server.use('/api/events', eventRoutes);
+server.use("/api/events", eventRoutes);
 userRoutes(server);
 
 module.exports = server;
