9ba1f859e4a9710efb1aee9a50790a2107342b9b
Louis Magdaleno <buildthatapp@gmail.com>
2019-08-31 21:43:42 -0700

    made map anon function async

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -23,7 +23,7 @@ async function fetchSchedules(eventId){
 router.get('/', async (req,res, next) => {
     try {
         const events = await db.findAll('Events');
-        events = events.map(event => {
+        events = events.map(async (event) => {
             let modifiedEvent = {...event, schedule : await fetchSchedules(event.id)};
             return modifiedEvent;
         })
