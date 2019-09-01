4579490d966acd3922563cba61f5ee48bccb15f2
Louis Magdaleno <buildthatapp@gmail.com>
2019-08-31 22:24:19 -0700

    updated db method to account for event id

--- data/models/index.js
+++ data/models/index.js
@@ -28,7 +28,7 @@ async function findAll(table) {
 async function findAllbyId(table, id){
 	let records;
 	try {
-		records = await db(table).where('id', id);
+		records = await db(table).where('event_id', id);
 	}
 	catch(err){
 		console.log(err);
