28b286b74b64357c90dce2f3238cd9cfabea785c
Louis Magdaleno <buildthatapp@gmail.com>
2019-09-05 20:17:44 -0700

    added skeleton api for salesRoutes

---
+++ routes/salesRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const db = require("../data/dbConfig");
+
+router.get('/', async () => {
+
+});
+
+router.get('/:saleId', async () => {
+
+});
+
+router.post('/', async () => {
+
+});
+
+router.put('/:saleId', async () => {
+
+});
+
+router.delete('/:saleId', async () => {
+
+});
+
+module.exports = router;
