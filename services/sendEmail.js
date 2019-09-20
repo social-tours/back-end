b138222d639358ffd2bda3c31b247c89e2a207d1
Rane <github@blasph.me>
2019-09-19 21:25:35 -0700

    Trying some things to fix spacing issues

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -3,37 +3,37 @@ const nodemailer = require("nodemailer");
 const sgTransport = require("nodemailer-sendgrid-transport");
 
 const sendMail = (recipients, message, subject) => {
-	const transporter = nodemailer.createTransport(
-		sgTransport({
-			auth: {
-				api_user: keys.sgUser,
-				api_key: keys.sgPW
-			}
-		})
-	);
+  const transporter = nodemailer.createTransport(
+    sgTransport({
+      auth: {
+        api_user: keys.sgUser,
+        api_key: keys.sgPW
+      }
+    })
+  );
 
-	const options = {
-		to: recipients,
-		from: keys.gmailName,
-		subject: subject || "An influencer you follow has a new event!",
-		html: message || "Log in for more details!" // TODO: Create email template
-	};
+  const options = {
+    to: recipients,
+    from: keys.gmailName,
+    subject: subject || "An influencer you follow has a new event!",
+    html: message || "Log in for more details!" // TODO: Create email template
+  };
 
-	try {
-		transporter.sendMail(options, (err, resp) => {
-			if (err) {
-				console.log("Error received from server: ", err);
-				return false;
-			} else {
-				console.log("Email sent!", resp);
-			}
-		});
-	} catch (err) {
-		console.log("Could not send email: ", err);
-		return false;
-	}
+  try {
+    transporter.sendMail(options, (err, resp) => {
+      if (err) {
+        console.log("Error received from server: ", err);
+        return false;
+      } else {
+        console.log("Email sent!", resp);
+      }
+    });
+  } catch (err) {
+    console.log("Could not send email: ", err);
+    return false;
+  }
 
-	return true;
+  return true;
 };
 
 module.exports = sendMail;
