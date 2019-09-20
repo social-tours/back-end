dd8c3383bf57d6773edecb99d311dd80b0ba44ab
Rane <github@blasph.me>
2019-09-19 20:05:35 -0700

    Created SendGrid service
    Service takes an array of email addresses and a message and sends the
    message to the email addresses. SendGrid API key is still needed to
    make it functional.

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,7 @@
 module.exports = {
 	sgApiKey: process.env.SG_API_KEY,
 	gmailName: process.env.GMAIL_NAME,
-	gmailPW: process.env.GMAIL_PW
+	gmailPW: process.env.GMAIL_PW,
+	sgUser: process.env.SG_USER,
+	sgPW: process.env.SG_PW
 };
---
+++ services/sendEmail.js
@@ -0,0 +1,35 @@
+const keys = require("../config/keys");
+const nodemailer = require("nodemailer");
+const sgTransport = require("nodemailer-sendgrid-transport");
+
+const sendMail = (recipients, message) => {
+	const transporter = nodemailer.createTransport(
+		sgTransport({
+			api_user: keys.sgUser,
+			api_key: keys.sgPW
+		})
+	);
+
+	const options = {
+		to: recipients,
+		from: keys.gmailName,
+		subject: "An influencer you follow has a new event!",
+		html: "Log in to view the event"
+	};
+
+	try {
+		transporter.sendMail(options, (err, resp) => {
+			if (err) {
+				console.log("Something went wrong: ", err);
+				return false;
+			} else {
+				console.log("Email sent!", resp);
+			}
+		});
+	} catch (err) {
+		console.log("Something went wrong: ", err);
+		return false;
+	}
+
+	return true;
+};
