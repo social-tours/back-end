fadb5414118eb596f093fece4e84df2f02db1b63
Louis Magdaleno <buildthatapp@gmail.com>
2019-09-19 21:53:23 -0700

    Added subscriptionRoutes

---
+++ routes/subscriptionRoutes.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+
+// Import data models
+const db = require("../data/models");
+
+// Load middleware
+
+/**
+ * Method to retrieve all subscriptions from the database
+ * @returns sends all subscriptions in the database as a response
+ */
+
+router.get("/", async (req, res) => {
+	try {
+		const data = await db.findAll("Subscriptions");
+		res.send(data);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+/**
+ * Method to retrieve a subscriptions for a specific user from the database
+ * @param {number} userId
+ * @returns sends the specific schedule as a response
+ */
+router.get("/:userId", async (req, res) => {
+	const { userId } = req.params;
+	try {
+		const data = await db.findAllbyId("Subscriptions", userId);
+		if (data) {
+			res.send(data);
+		} else {
+			res.status(404).json({ message: `Record ${userId} not found` });
+		}
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+module.exports = Router;
