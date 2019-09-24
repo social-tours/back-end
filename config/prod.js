f20fa25c4989cf5a10d8e20194d14d81d78395f3
Rane <github@blasph.me>
2019-09-23 19:15:24 -0700

    Removed an unneeded env variable
     I have also added all of the SendGrid/Gmail env variables to the
     production and staging environments on Heroku

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,6 @@
 module.exports = {
-	sgApiKey: process.env.SG_API_KEY,
-	gmailName: process.env.GMAIL_NAME,
-	gmailPW: process.env.GMAIL_PW,
-	sgUser: process.env.SG_USER,
-	sgPW: process.env.SG_PW
+  gmailName: process.env.GMAIL_NAME,
+  gmailPW: process.env.GMAIL_PW,
+  sgUser: process.env.SG_USER,
+  sgPW: process.env.SG_PW
 };
