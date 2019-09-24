322f0d07dacefcfdd738384b74085a0db47c5ec4
Rane <github@blasph.me>
2019-09-23 19:26:36 -0700

    Adding Travis check to keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 // keys.js - determine correct credentials
-if (process.env.NODE_ENV === "production") {
-	// in prod
-	module.exports = require("./prod");
+if (process.env.NODE_ENV === "production" && !process.env.TRAVIS) {
+  // in prod
+  module.exports = require("./prod");
 } else {
-	// in dev
-	module.exports = require("./dev");
+  // in dev
+  module.exports = require("./dev");
 }
