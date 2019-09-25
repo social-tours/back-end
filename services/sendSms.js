085f252ea837f66fa5268fb7978d02fc08fb8ba4
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-09-25 18:07:55 +0800

    Add service to send SMS messages

---
+++ services/sendSms.js
@@ -0,0 +1,23 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+/**
+ * Function which takes a message and
+ * sends it via SMS to specified phone number
+ * @param {string} message
+ * @param {e.164 phone number format} userPhone
+ */
+async function sendText(message, userPhone) {
+	try {
+		sendMessage = await client.messages.create({
+			body: message,
+			from: process.env.TWILIO_PHONE_NBR,
+			to: userPhone
+		});
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+module.exports = sendText;
