a1ef852312fa687dddc8fcb9db38347e5283a9e6
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-09-27 13:25:45 +0800

    Update test events data

--- data/seeds/007_event_tables.js
+++ data/seeds/007_event_tables.js
@@ -14,12 +14,35 @@ exports.seed = async function(knex) {
 	await knex("Events").insert([
 		{
 			id: 1,
-			type: 1, // FK ID in 'EventTypes' table
-			host_id: 2,
+			type: 1,
 			title: "See Bifunkal Orchestra",
 			description: "Orchestrated Blues and Funk",
 			event_image: "Bifunkal Image Here",
 			capacity: 5000
+		},
+		{
+			id: 2,
+			type: 1,
+			title: "TEDx Muncie",
+			description: "TEDx event in Muncie, Indiana",
+			event_image: "TEDx logo",
+			capacity: 350
+		},
+		{
+			id: 3,
+			type: 1,
+			title: "Hair and back again with Kylie Jenner",
+			description: "Kylie Jenner Fan Club Meetup",
+			event_image: "KJ logo",
+			capacity: 3000
+		},
+		{
+			id: 4,
+			type: 1,
+			title: "Nerd Fitness Bootcamp",
+			description: "Nerd Fitness Reloaded Bootcamp Tour",
+			event_image: "NF logo url",
+			capacity: 50
 		}
 	]);
 
