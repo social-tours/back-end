60536f191755540985fd9b990aab9346c17245e1
Louis Magdaleno <buildthatapp@gmail.com>
2019-10-06 20:54:59 -0700

    added search param to route

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
+import db from '../data/models'
 
-router.get('/search', (req,res) => {
+router.get('/search/:name', async (req, res) => {
+    let { name } = req.params;
+    try {
+        if (name !== 'undefined') {
+            let records = await db.search(name);
 
-});
+            res.status(200).send(records);
+        } else {
+            res.send([]);
+        }
+    } catch (e) {
+        res.send(e);
+    }
+})
 
 module.exports = router;
