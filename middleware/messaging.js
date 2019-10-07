db406d3b1381b8d0e03dcbaba65cc06ffe3a8540
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-10-07 13:25:58 +0800

    Refactor service selection to conditional statements
    Add function documentation

--- middleware/messaging.js
+++ middleware/messaging.js
@@ -4,13 +4,14 @@ const sendEmail = require("../services/sendEmail");
 const sendSms = require("../services/sendSms");
 
 /**
- *
- * @param {*} influencerId
- * @param {*} content
+ * Function to retrieve and filter list of
+ * followers based on communication preferences
+ * @param {integer} influencerId
+ * @param {string} content
  */
 async function messagingHandler(influencerId, content) {
 	try {
-		const data = await db("Users")
+		const users = await db("Users")
 			.innerJoin("Subscriptions", "Users.id", "Subscriptions.user_id")
 			.select(
 				"Users.id",
@@ -21,11 +22,11 @@ async function messagingHandler(influencerId, content) {
 				"Subscriptions.influencer_id",
 				"Users.comm_preference"
 			)
-			.where({ influencer_id: influencerId })
+			.where({ influencer_id: influencerId, marketing_opt_in: true })
 			.whereNot({ comm_preference: 0 });
-		console.log(`messagingHandler ${content}: `, data);
+		console.log(`messagingHandler ${content}: `, users);
 
-		selectMessagingService(data, content);
+		selectMessagingService(users, content);
 	} catch (error) {
 		console.error(error);
 		return { message: "Database query error occured", error };
@@ -33,52 +34,33 @@ async function messagingHandler(influencerId, content) {
 }
 
 /**
- *
+ * Helper function to route and call messaging service
+ * based on user's communication preferences
  * @param {array} users
  * @param {string} content
  */
 function selectMessagingService(users, content) {
 	for (let user of users) {
-		switch (user.comm_preference) {
-			case 1:
-				if (user.email) {
-					console.log(`send email to ${user.email}: ${content}`);
-					sendEmail(user.email, content);
-				} else {
-					console.log("Invalid email address");
-					return "Invalid email address";
-				}
-
-			case 2:
-				if (user.phone_nbr) {
-					console.log(`send sms to ${user.phone_nbr}: ${content}`);
-					sendSms(user.phone_nbr, content);
-				} else {
-					console.log("Invalid phone number");
-					return "Invalid phone number";
-				}
-
-			case 3:
-				if (user.email && user.phone_nbr) {
-					console.log(
-						`send to both ${user.email} and ${user.phone_nbr}: ${content}`
-					);
-					sendEmail(user.email, content);
-					sendSms(user.phone_nbr, content);
-				} else if (user.email) {
-					console.log(`send email to ${user.email}: ${content}`);
-					sendEmail(user.email, content);
-				} else if (user.phone_nbr) {
-					console.log(`send sms to ${user.phone_nbr}: ${content}`);
-					sendSms(user.phone_nbr, content);
-				} else {
-					console.log("Invalid phone number");
-					return "Invalid phone number";
-				}
-
-			default:
-				console.log("no message sent");
-				return;
+		if (user.comm_preference === 0) {
+			console.log("Will not send notification");
+			return "Will not send notification";
+		} else if (user.comm_preference === 3) {
+			if (user.email) {
+				sendEmail(user.email, content);
+			}
+			if (user.phone_nbr) {
+				sendSms(user.phone_nbr, content);
+			}
+		} else if (user.comm_preference === 2) {
+			if (user.phone_nbr) {
+				sendSms(user.phone_nbr, content);
+			}
+		} else if (user.comm_preference === 1) {
+			if (user.email) {
+				sendEmail(user.email, content);
+			}
+		} else {
+			console.log("Unspecified communication preferences");
 		}
 	}
 }
